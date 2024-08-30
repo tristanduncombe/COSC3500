7b2b928f2eabd02a55eef516c0791d44a8cfdd60
Tristan Duncombe <tduncombe747@gmail.com>
2024-08-30 22:20:47 +1000

    Update visualise

--- Milestone 1/visualise.py
+++ Milestone 1/visualise.py
@@ -19,23 +19,22 @@ point_colors = [random.choice(colors) for _ in range(frames.shape[1])]
 
 # Initialize previous frame as None
 previous_frame = None
-
+image_files = []
 # Plot each frame
-for i, frame in enumerate(frames):
+for i in range(0, len(frames), 10):
+    frame = frames[i]
     fig = plt.figure(figsize=(10, 8))  # Adjust the figure size
     ax = fig.add_subplot(111, projection='3d')
     x, y, z = frame[:, 0], frame[:, 1], frame[:, 2]
-    
+
     # Use the assigned colors for the points
     ax.scatter(x, y, z, color=point_colors)
-    
     # If there is a previous frame, draw lines between the points
     if previous_frame is not None:
         prev_x, prev_y, prev_z = previous_frame[:, 0], previous_frame[:, 1], previous_frame[:, 2]
         for j in range(len(frame)):
             ax.plot([prev_x[j], x[j]], [prev_y[j], y[j]], [prev_z[j], z[j]], color='r')
-    
-    ax.set_title(f'Time={i * 0.01:.2f}')
+    ax.set_title(f'Time={i * 0.0001:.4f}')
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
@@ -43,19 +42,20 @@ for i, frame in enumerate(frames):
     ax.set_ylim(-10, 10)
     ax.set_zlim(-10, 10)
     plt.grid(True)
+    filename = f'./frames/frame_{i}.png'
     plt.savefig(f'./frames/frame_{i}.png')
     plt.close()
-    
-    # Update previous frame
+    image_files.append(filename)
     previous_frame = frame
 
 # List to store the file names of the generated images
-image_files = [f'./frames/frame_{i}.png' for i in range(len(frames))]
 
-# Read images and store them in a list
+
+# Create a list to store the images
 images = []
-for filename in image_files:
-    images.append(imageio.imread(filename))
 
 # Save the images as a GIF
-imageio.mimsave('animation.gif', images, duration=0.5)
+with imageio.get_writer('animation.gif', mode='I') as writer:
+    for filename in image_files:
+        img = imageio.imread(filename)
+        writer.append_data(img, {'duration': 0.1})
